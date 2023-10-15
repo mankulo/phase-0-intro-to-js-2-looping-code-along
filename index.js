c3e878d696fd5f30160676e8b7edacd206fbb496
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-10-15 21:10:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, reaction){
+    const newName = []
+    for(let i = 0 ; i < name.length; i++){
+    newName.push(`Thank you, ${name[i]}, for the wonderful ${reaction} gift!`);
+    }
+    return newName;
+}
+
+function countDown(number){
+    for(let i=number;i>=0;i--){
+        console.log(i)
+    }
+    }
+countDown(11)
